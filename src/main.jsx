7b5cf3137d20f4ef99e84a81742d8925ae14edcc
DeniseLara <denise2001.lara@gmail.com>
2025-03-05 14:54:41 -0300

    cambios

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'; // Importa BrowserRouter
-import ReactDOM from 'react-dom/client';
 import App from './App.jsx'
 
-//createRoot(document.getElementById('root')).render(
 const root = createRoot(document.getElementById('root'));
 root.render(
   <StrictMode>
