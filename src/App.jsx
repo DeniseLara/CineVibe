10c06f520ab7e9095ebddfb321e66b5de28685ae
DeniseLara <denise2001.lara@gmail.com>
2025-06-10 16:21:42 -0400

    Cambio temporal

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,10 @@ import { useDarkMode } from './hooks/useDarkMode';
 import HomePage from './pages/HomePage';
 import SearchPage from './pages/SearchPage';
 import FavoritePage from './pages/FavoritesPage';
-import Navbar from './components/Navbar/Navbar';
+import Navbar from './components/navbar__/Navbar';
 import AboutPage from './pages/AboutPage';
 import Movie from './pages/MovieDetails';
-import Header from './components/Header/Header';
+import Header from './components/header__/Header';
 
 function App() {
   const location = useLocation(); // Detectar la ruta actual
