56878e4b669bcb70880dbe835e711057375100a8
DeniseLara <denise2001.lara@gmail.com>
2025-06-10 16:02:25 -0400

    actualizaciones

--- src/components/moviedetails/ActorsList.jsx
+++ src/components/moviedetails/ActorsList.jsx
@@ -1,13 +1,13 @@
 function ActorsList({ actors }) {
   return (
-    <>
+    <section>
       <h3 className="movie-actors">Main Actors</h3>
       <ul className="movie-actors-list">
         {actors.map(actor => (
           <li className='movie-actor' key={actor.id}>{actor.name}</li>
         ))}
       </ul>
-    </>
+    </section>
   );
 }
 
